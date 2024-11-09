376664784169eca8de90d4816e8159111693d160
Tejas-Joshi-28 <tejas.joshi@sarvatra.in>
2024-11-09 17:50:14 +0530

    Just Checking-2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the Value :- ");
-        int a = sc.nextInt();
-        System.out.println("The value is :- " + a);
-
-        sc.close();
+        System.out.println("Hello World");
     }
 }
