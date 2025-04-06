d54d4297a8533daaff28a0258001ec140d10003e
Tejas-Joshi-17 <joshitejas188@gmail.com>
2025-04-06 11:03:22 +0530

    InstanceOfOperator added & Updated

---
+++ src/important/concepts/InstancOfOperator1.java
@@ -0,0 +1,78 @@
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+package important.concepts;
+
+class Head {
+    protected String head;
+}
+
+class Txn extends Head{
+    protected String txn;
+}
+
+class PayerAndPayer extends Txn {
+    protected String payer;
+    protected String payee;
+}
+
+class ReqPay extends PayerAndPayer {
+    protected String reqPay;
+}
+
+public class InstancOfOperator1 {
+
+    public static void main(String[] args) {
+
+        ReqPay reqPay = new ReqPay();
+
+        if(reqPay instanceof ReqPay reqPay1) {                  // <---- Always true i.e. Giving Warning, please Ignore
+            parseReqPay(reqPay1);
+        }
+
+        if(reqPay instanceof PayerAndPayer payerAndPayer) {     // <---- Always true i.e. Giving Warning, please Ignore
+            parsePayerAndPayer(payerAndPayer);
+        }
+
+        if(reqPay instanceof Txn txn) {                         // <---- Always true i.e. Giving Warning, please Ignore
+            parseTxn(txn);
+        }
+
+        if(reqPay instanceof Head head) {                       // <---- Always true i.e. Giving Warning, please Ignore
+            parseHead(head);
+        }
+    }
+
+    static void parseReqPay(ReqPay reqPay) {
+        reqPay.head = "HEAD";
+        reqPay.txn = "TXN";
+        reqPay.payer = "PAYER";
+        reqPay.payee = "PAYEE";
+        reqPay.reqPay = "REQ_PAY";
+    }
+
+    static void parsePayerAndPayer(PayerAndPayer payerAndPayer) {
+        payerAndPayer.head = "HEAD";
+        payerAndPayer.txn = "TXN";
+        payerAndPayer.payer = "PAYER";
+        payerAndPayer.payee = "PAYEE";
+        // payerAndPayer.reqPay = "REQ_PAY";       <------ Cannot resolve symbol 'reqPay'
+    }
+
+    static void parseTxn(Txn txn) {
+        txn.head = "HEAD";
+        txn.txn = "TXN";
+        // txn.payer = "PAYER";          <------ Cannot resolve symbol 'payer'
+        // txn.payee = "PAYEE";          <------ Cannot resolve symbol 'payee'
+        // txn.reqPay = "REQ_PAY";       <------ Cannot resolve symbol 'reqPay'
+    }
+
+    static void parseHead(Head head) {
+        head.head = "HEAD";
+        // head.txn = "TXN";             <------ Cannot resolve symbol 'txn'
+        // txn.payer = "PAYER";          <------ Cannot resolve symbol 'payer'
+        // txn.payee = "PAYEE";          <------ Cannot resolve symbol 'payee'
+        // txn.reqPay = "REQ_PAY";       <------ Cannot resolve symbol 'reqPay'
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------
--- src/important/concepts/InstanceOfOperator.java
+++ src/important/concepts/InstanceOfOperator.java
@@ -12,7 +12,7 @@ class Parent {
     int parentVariable;
 
     public void funcParents() {
-        logger.info("Function - Child Class");
+        logger.info("Function - Parent Class");
     }
 
     void getParentVariable() {
@@ -39,29 +39,29 @@ public class InstanceOfOperator {
     
     public static void main(String[] args) {
 
-        Parent p1 = new Parent();
-        logger.info(String.valueOf(p1 instanceof Parent));       // true
-        logger.info(String.valueOf(p1 instanceof Child));        // false
+        Parent parent1 = new Parent();
+        logger.info(String.valueOf(parent1 instanceof Parent));       // true
+        logger.info(String.valueOf(parent1 instanceof Child));        // false
 
-        Child c1 = new Child();
-        logger.info(String.valueOf(c1 instanceof Parent));       // true
-        logger.info(String.valueOf(c1 instanceof Child));        // true
+        Child child1 = new Child();
+        logger.info(String.valueOf(child1 instanceof Parent));        // true
+        logger.info(String.valueOf(child1 instanceof Child));         // true
 
-        Parent p2 = new Child();
-        logger.info(String.valueOf(p2 instanceof Parent));       // true
-        logger.info(String.valueOf(p2 instanceof Child));        // true
-        p2.parentVariable = 12;
+        Parent parent2 = new Child();
+        logger.info(String.valueOf(parent2 instanceof Parent));       // true
+        logger.info(String.valueOf(parent2 instanceof Child));        // true
+        parent2.parentVariable = 12;
 
-        // Child c2 = new Child();       <----- Error
+        // Child c2 = new Parent();       <----- Error
 
-        Child c2 = (Child) p2;
-        c2.getParentVariable();                       // Value of parentVariable is :- 12
-        c2.parentVariable = 10;
-        c2.childVariable = 20;
-        c2.getParentVariable();                       // Value of parentVariable is :- 10
-        c2.getChildVariable();                        // Value of childVariable is :- 20
-        c2.funcParents();                             // Function - Child Class
-        c2.funcChild();                               // Function - Child Class
+        Child child2 = (Child) parent2;
+        child2.getParentVariable();                       // Value of parentVariable is :- 12
+        child2.parentVariable = 10;
+        child2.childVariable = 20;
+        child2.getParentVariable();                       // Value of parentVariable is :- 10
+        child2.getChildVariable();                        // Value of childVariable is :- 20
+        child2.funcParents();                             // Function - Parent Class
+        child2.funcChild();                               // Function - Child Class
 
     }
 }
